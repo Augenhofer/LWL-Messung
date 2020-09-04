c8004e24b206d5c76c4a7b90436fcf28027a4ec9
Augenhofer <35490144+Augenhofer@users.noreply.github.com>
2020-09-04 07:36:49 +0200

    Projektdateien hinzufügen.

---
+++ LWL-Messung/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LWL_Messung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
